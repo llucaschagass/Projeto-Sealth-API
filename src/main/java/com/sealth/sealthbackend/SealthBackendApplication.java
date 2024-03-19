4fa8b42f024677e9b4ed86bcc13b06540ad97f26
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2024-03-19 15:10:38 -0300

    first commit

---
+++ src/main/java/com/sealth/sealthbackend/SealthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sealth.sealthbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SealthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SealthBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sealth/sealthbackend/SealthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sealth.sealthbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SealthBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
