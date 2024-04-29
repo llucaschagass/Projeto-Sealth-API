c3873c9133cc50e92882fc3a4d77708be6716b22
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2024-04-29 15:32:50 -0300

    feat: criando a Controller Medicines

---
+++ sealth-api/src/main/java/com/sealth/sealthapi/controllers/MedicinesController.java
@@ -0,0 +1,29 @@
+package com.sealth.sealthapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@RestController
+public class MedicinesController {
+
+    private final List<String> pdfList = Arrays.asList("paracetamol", "omeprazol", "amoxicilina", "atorvastatina", "metformina");
+
+    @GetMapping("/medicines/pdf")
+    public List<String> getRandomPDFs() {
+        List<String> selectedPDFs = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++) {
+            int index = random.nextInt(pdfList.size());
+            String selectedPDF = pdfList.get(index);
+            selectedPDFs.add(selectedPDF);
+        }
+
+        return selectedPDFs;
+    }
+}
