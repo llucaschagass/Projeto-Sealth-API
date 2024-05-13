1a4423b7a142a8742cd189de4c06b0e880cd339e
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2024-05-13 13:38:17 -0300

    bug: removendo random para retornar toda a lista

--- sealth-api/src/main/java/com/sealth/sealthapi/controllers/MedicinesController.java
+++ sealth-api/src/main/java/com/sealth/sealthapi/controllers/MedicinesController.java
@@ -4,27 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 @RestController
 public class MedicinesController {
 
-    private final List<String> pdfList = Arrays.asList("Paracetamol", "Omeprazol", "Amoxicilina", "Atorvastatina", "Metformina");
+    // Lista de todos os medicamentos disponíveis
+    private final List<String> allMedicines = Arrays.asList("Paracetamol", "Omeprazol", "Amoxicilina", "Atorvastatina", "Metformina");
 
     @GetMapping("/medicines/pdf")
-    public List<String> getRandomPDFs() {
-        List<String> selectedPDFs = new ArrayList<>();
-        Random random = new Random();
-
-        for (int i = 0; i < 3; i++) {
-            int index = random.nextInt(pdfList.size());
-            String selectedPDF = pdfList.get(index);
-            selectedPDFs.add(selectedPDF);
-        }
-
-        return selectedPDFs;
+    public List<String> getAllPDFs() {
+        // Retorna a lista completa de medicamentos
+        return allMedicines;
     }
 }
